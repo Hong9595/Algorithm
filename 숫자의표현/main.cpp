91753650947bdab718bc4ad9f514af7c6f6c9e41
Hong9595 <ahk959595@gmail.com>
2020-03-12 10:22:46 +0900

    Lv2.숫자의 표현

---
+++ 숫자의표현/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    
+    int start = 1, end = 1;
+    int acc = 0;
+    while(start <= n || end > n) {
+        if(acc > n) {
+            acc -= start;
+            start++;
+            continue;
+        }
+        if(acc == n)    answer++;
+        
+        acc += end;
+        end++;
+    }
+    
+    return answer;
+}
+int main(int argc, const char * argv[]) {
+    cout<<solution(15);
+    return 0;
+}
